17efc6fec967f6f30bbcae11dbd207ea75355d4a
Alok Raj <91530763+pgicaalok05@users.noreply.github.com>
2023-06-04 23:36:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-pickle_in = open("/content/drive/My Drive/Colab Notebooks/NSP_Creditcardfrauddetection.pkl","rb")
+pickle_in = open("NSP_Creditcardfrauddetection.pkl","rb")
 model=pickle.load(pickle_in)
 dataset= pd.read_csv('/content/drive/My Drive/Colab Notebooks/Dataset/card_transdata.csv')
 X = dataset.iloc[:, 0:8].values
